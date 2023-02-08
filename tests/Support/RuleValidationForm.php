4e3d37231db3fbec50f7d01a6feff0f276d77a20
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-02-08 09:25:44 -0300

    Add ability get html attributes from rules validator. (#5)

---
+++ tests/Support/RuleValidationForm.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yii\Widget\Tests\Support;
+
+use Yii\FormModel\AbstractFormModel;
+
+final class RuleValidationForm extends AbstractFormModel
+{
+    private array $array = [];
+    private string|null $string = '';
+
+    public function getRuleOptionsAttribute(string $attribute): array
+    {
+        return match ($attribute) {
+            'string' => ['required' => true],
+            default => [],
+        };
+    }
+}
